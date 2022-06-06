1e32269ed3f75c6949c07eee2861322c60f457d2
kajela <titoyut56@gmail.com>
2022-06-06 16:02:58 -0400

    add the initial files

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class City {
+    private   String name = "nairobi";
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        City myObj = new City();
+        myObj.setName("Nairobi");
+
+        System.out.println(myObj.getName());
     }
 }
